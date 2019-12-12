bc2c52c067adcc7b07ac7b95324d666563564db3
seolryuhee <sagittarius.snowflake@gmail.com>
2019-12-12 21:13:56 +0900

    에러 발생

---
+++ src/main/java/com/park/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.park.book.springboot.web;
+
+import com.park.book.springboot.service.posts.PostsService;
+import com.park.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/vi/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
