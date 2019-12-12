d7b8ec68a16c4297ae61cce5d49457447398a24c
seolryuhee <sagittarius.snowflake@gmail.com>
2019-12-12 17:53:56 +0900

    test

---
+++ src/main/java/com/park/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.park.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+// 클래스 내 모든 필드의 Getter 메서드 자동 생성
+@Getter
+// 기본 생성자 자동 추가
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    //PK 필드 선언
+    @Id
+    // PK 생성 규칙(auto_increment)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //column 변경하고 싶을 때 사용
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    //해당 클래스의 빌더 패턴 클래스 생성
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
